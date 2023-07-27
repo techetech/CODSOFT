ba9f8c11bede21e006217a89da4773fd5e982446
Harsh Jadon <76236737+techetech@users.noreply.github.com>
2023-07-27 08:56:50 +0530

    .

--- password-generator.py
+++ password-generator.py
@@ -7,5 +7,5 @@ symbol=string.punctuation
 char=letters+digits+symbol
 
 length=int(input("Enter the desired length of password : "))
-password= "".join(choice(chars) for x in range(randint(length)))
+password= "".join(choice(char) for x in range(0,length))
 print(password)
